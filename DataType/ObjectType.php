c78870e13589653a23ea4cca222174b62d569a00
andrey <andrey@4devs.pro>
2016-10-27 15:10:42 +0300

    Update ObjectType.php

--- DataType/ObjectType.php
+++ DataType/ObjectType.php
@@ -2,13 +2,14 @@
 
 namespace FDevs\Serializer\DataType;
 
+use FDevs\Serializer\Exception\MappingException;
 use FDevs\Serializer\Normalizer\DenormalizerAwareInterface;
 use FDevs\Serializer\Normalizer\DenormalizerAwareTrait;
 use FDevs\Serializer\Normalizer\NormalizerAwareInterface;
 use FDevs\Serializer\Normalizer\NormalizerAwareTrait;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ObjectType extends AbstractType implements DenormalizerAwareInterface , NormalizerAwareInterface
+class ObjectType extends AbstractType implements DenormalizerAwareInterface, NormalizerAwareInterface
 {
     use DenormalizerAwareTrait;
     use NormalizerAwareTrait;
@@ -20,7 +21,12 @@ class ObjectType extends AbstractType implements DenormalizerAwareInterface , No
      */
     public function denormalize($data, array $options, array $context = [])
     {
-        return $this->getDenormalizer()->denormalize($data, $options['data_class'], $options['format'], $context);
+        if (!$options['data_class'] && !isset($context[$options['key']])) {
+            throw new MappingException(sprintf('set "data_class" or set context with key "%s"', $options['key']));
+        }
+        $dataClass = $options['data_class'] ?: $context[$options['key']];
+
+        return $this->getDenormalizer()->denormalize($data, $dataClass, $options['format'], $context);
     }
 
     /**
@@ -43,13 +49,15 @@ class ObjectType extends AbstractType implements DenormalizerAwareInterface , No
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver
-            ->setRequired(['data_class'])
-            ->setDefined(['max_depth', 'format'])
+            ->setDefined(['max_depth', 'format', 'data_class', 'key'])
             ->setDefaults([
                 'max_depth' => 2,
                 'format' => null,
+                'data_class' => null,
+                'key' => 'data_class',
             ])
             ->addAllowedTypes('data_class', ['string'])
+            ->addAllowedTypes('key', ['string'])
             ->addAllowedTypes('max_depth', ['integer'])
             ->addAllowedTypes('format', ['integer', 'null']);
     }
