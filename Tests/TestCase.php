beb5a154fc12aa321af66c9b6aa4544e7a6e8d54
andrey1s <andrey@4devs.io>
2017-03-23 22:58:22 +0300

    use phpunit 6.0

--- Tests/TestCase.php
+++ Tests/TestCase.php
@@ -2,6 +2,8 @@
 
 namespace FDevs\Serializer\Tests;
 
-class TestCase extends \PHPUnit_Framework_TestCase
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+class TestCase extends BaseTestCase
 {
 }
