abd6727fc63e41c1a7cfe7ea70370e7f7e94c858
andrey1s <andrey@4devs.pro>
2018-01-25 10:11:09 +0300

    ser require php cs

--- Mapping/PropertyMetadata.php
+++ Mapping/PropertyMetadata.php
@@ -1,5 +1,12 @@
 <?php
 
+/*
+ * This file is part of the 4devs Serialiser package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace FDevs\Serializer\Mapping;
 
 class PropertyMetadata extends Metadata implements PropertyMetadataInterface
@@ -80,11 +87,13 @@ class PropertyMetadata extends Metadata implements PropertyMetadataInterface
 
     /**
      * @param MetadataInterface $visibility
+     *
      * @return $this
      */
     public function addVisibility(MetadataInterface $visibility): self
     {
         $this->visibility[] = $visibility;
+
         return $this;
     }
 
